d9377fc06547b332eeab244e0ec535f101be1dca
SeWa <sebastian_walter@me.com>
2018-07-22 14:16:32 +0200

    Changes message again

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello OpenShift!')
+    res.send('Hello OpenShift! With new naming strategy :)')
 })
 
 app.listen(3000, () => {
