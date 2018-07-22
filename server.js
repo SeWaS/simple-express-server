42545d12aa9b515ace740a51af375bfd30974d28
SeWa <sebastian_walter@me.com>
2018-07-22 14:29:50 +0200

    Changes message (to the final one?)

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello OpenShift! With new naming strategy :)')
+    res.send('Hello OpenShift! Is this the final version?')
 })
 
 app.listen(3000, () => {
