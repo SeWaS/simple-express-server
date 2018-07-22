4de9b88a109cc571231861522b16d38c0f499f3e
SeWa <sebastian_walter@me.com>
2018-07-22 14:52:10 +0200

    Message changed again!?

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello OpenShift! Is this the final version?')
+    res.send('Hello World!')
 })
 
 app.listen(3000, () => {
